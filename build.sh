7eaac11b3171b699d497f0fd18c49509a1b0295f
Thibault Pierre <tib.pierre@gmail.com>
2013-07-17 14:26:42 +0200

    add bower + bootstrap

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# MyApplication build script
+
+php composer.phar install --dev
+
+# create database if not exists
+php app/console doctrine:database:create --quiet --env=${1:-dev}
+
+php app/console doctrine:schema:drop --force --env=${1:-dev}
+php app/console doctrine:schema:update --force --env=${1:-dev}
+
+#php app/console doctrine:fixtures:load  --no-interaction --env=${1:-dev}
+
+php app/console cache:clear --env=test --env=${1:-dev}
+php app/console cache:warmup --env=test --env=${1:-dev}
+
+bower install
